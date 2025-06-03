87bcc60740453e979e7e63f52303dd525c205e2d
SbWereWolf <ulfnew@gmail.com>
2025-06-03 05:46:36 +0500

    Add links for entities CRUD

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -2,50 +2,72 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+use yii\helpers\Url;
+
+$this->title = 'Расписание поездок';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+    <div class="jumbotron">
+        <h1>Расписания поездок</h1>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            <div class="col-lg-2">
+                <h2>Journey</h2>
+                <p>Поездки</p>
+                <p><a
+                            class="btn btn-default"
+                            href="<?= Url::to(['journey/index']); ?>">
+                        Работать с Поездками
+                    </a></p>
             </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <div class="col-lg-2">
+                <h2>Companion</h2>
+                <p>Участники поездок</p>
+                <p><a
+                            class="btn btn-default"
+                            href="<?= Url::to(['companion/index']); ?>">
+                        Работать с Участниками поездок
+                    </a></p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-lg-2">
+                <h2>Timetable Slot</h2>
+                <p>Пункты в расписании</p>
+                <p><a
+                            class="btn btn-default"
+                            href="<?= Url::to(['timetable-slot/index']); ?>">
+                        Работать с Пунктами в расписании
+                    </a></p>
+            </div>
+            <div class="col-lg-2">
+                <h2>Transfer Service</h2>
+                <p>Перемещения из точки А в точку Б</p>
+                <p><a
+                            class="btn btn-default"
+                            href="<?= Url::to(['transfer-service/index']); ?>">
+                        Работать с Перемещениями
+                    </a></p>
+            </div>
+            <div class="col-lg-2">
+                <h2>Hotel Service</h2>
+                <p>Размещения для проживания</p>
+                <p><a
+                            class="btn btn-default"
+                            href="<?= Url::to(['hotel-service/index']); ?>">
+                        Работать с Проживаниями
+                    </a></p>
+            </div>
+            <div class="col-lg-2">
+                <h2>Service Timetable Slot</h2>
+                <p>Занятость в соответствии с пунктами расписания</p>
+                <p><a
+                            class="btn btn-default"
+                            href="<?= Url::to(['service-timetable-slot/index']); ?>">
+                        Работать с Занятостью по расписанию
+                    </a></p>
             </div>
         </div>
 
